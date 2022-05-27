1c2729263239d9688a824cf7c000bf7eaa5cb738
Aimarest <amarcosestevez@gmail.com>
2022-05-27 18:29:33 +0200

    add states const

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,5 +1,13 @@
 import { Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+//Services:
+import apiExercises from "../services/api-exercises";
+import apiUsers from "../services/api-users";
+import apiKitchenRecipes from "../services/api-kitchen-recipes";
+import router from "../services/router";
+import ls from "../services/local-storage";
 //Components:
+import Beginning from "./Beginning";
 import Header from "./Header";
 import Login from "./Login";
 import SignUp from "./SignUp";
@@ -10,37 +18,52 @@ import MyKitchenRecipes from "./MyKitchenRecipes";
 import MyTrainingExercises from "./MyTrainingExercises";
 import Footer from "./Footer";
 
-function App() {
+const App = () => {
+  // state: user
+  const identification = ls.get("userId", "");
+  const [userId, setUserId] = useState(identification);
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userKitchenRecipes, setUserKitchenRecipes] = useState([]);
+  const [userTrainingExercises, setUserTrainingExercises] = useState([]);
+  const [allKitchenRecipes, setAllKitchenRecipes] = useState([]);
+  const [allTrainingExercises, setAllTrainingExercises] = useState([]);
+  /*
+  Event: cerrar sesión.
+  Redireccionamos al inicio de la página.
+  Recargamos la página para que se borren todos los datos del estado de React.
+  */
+  const logout = () => {
+    router.redirect("/");
+    router.reload();
+    // he añadido esta sentencia para deslogear a la usuaria cuando pulsa el botón de 'Cerrar sesión'
+    setUserId("");
+  };
+
   return (
     <div className="App">
+      <Header isUserLogged={!!userId} logout={logout} />
       <Switch>
         <Route exact path="/">
-          <Header>
-            <Route path="/login">
-              <Login
-                loginErrorMessage={loginErrorMessage}
-                sendLoginToApi={sendLoginToApi}
-              />
-            </Route>
-            <Route path="/signup">
-              <SignUp
-                signUpErrorMessage={signUpErrorMessage}
-                sendSingUpToApi={sendSingUpToApi}
-              />
-            </Route>
-            <Route path="/profile">
-              <Profile
-                userName={userName}
-                userEmail={userEmail}
-                userPassword={userPassword}
-                sendProfileToApi={sendProfileToApi}
-              />
-            </Route>
-          </Header>
+          <Beginning />{" "}
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/profile">
+          <Profile
+            userName={userName}
+            userEmail={userEmail}
+            userPassword={userPassword}
+          />
         </Route>
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ web/src/components/Beginning.js
@@ -0,0 +1,13 @@
+const Beginning = () => {
+  return (
+    <div>
+      <h1>Welcome to the fitness life app!!</h1>
+      <h2>If you like to take care of yourself, you will love this app.</h2>
+      <h3>
+        Here you will find different exercises for your daily training and
+        several healthy cooking recipes.
+      </h3>
+    </div>
+  );
+};
+export default Beginning;
